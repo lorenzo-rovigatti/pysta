1b8c64d5137b0863d4d3715b258eb094f9c2b4d1
Lorenzo Rovigatti <lorenzo.rovigatti@gmail.com>
2020-01-10 23:33:36 +0100

    Use the curses lib to manage the user input

--- src/client.py
+++ src/client.py
@@ -5,27 +5,65 @@ Created on 19 dic 2019
 '''
 
 import asyncio
+import curses
+import time
+
+UPDATE_DISTANCE_EVERY = 1
 
 @asyncio.coroutine
-def tcp_echo_client(message, loop):
+def send_command(message, loop, win):
     reader, writer = yield from asyncio.open_connection('192.168.1.141', 8888, loop=loop)
 
-    print('Send: %r' % message)
     writer.write(message.encode())
-
     data = yield from reader.read(100)
-    print('Received: %r' % data.decode())
-
-    print('Close the socket')
     writer.close()
+    
+    win.clear()
+    win.addstr(str(data.decode()), curses.color_pair(1))
+    win.refresh()
+    
 
-message = 'Hello World!'
-loop = asyncio.get_event_loop()
+def main(stdscr):
+    # make calls to getkey() non blocking
+    stdscr.nodelay(True)
+    # disable the cursor
+    curses.curs_set(False)
+    # enable terminal colours
+    curses.start_color()
+    # define the status color pair
+    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_WHITE)
+    
+    status_window = curses.newwin(1, curses.COLS - 1, 0, 0)
+    distance_window = curses.newwin(1, curses.COLS - 1, 1, 0)
+    
+    loop = asyncio.get_event_loop()
 
-try:
-    while True:
-        command = input()
-        if len(command) > 0:
-            loop.run_until_complete(tcp_echo_client(command, loop))
-finally:
-    loop.close()
+    next_distance_time = time.time() + UPDATE_DISTANCE_EVERY
+    try:
+        while True:
+            try:
+                key = stdscr.getkey()
+                
+                command = False
+                if key == "w":
+                    command = "forward"
+                elif key == "a":
+                    command = "left"
+                elif key == "d":
+                    command = "right"
+                elif key == "s":
+                    command = "stop"
+                elif key == "r":
+                    command = "reverse"
+                
+                if command != False:
+                    loop.run_until_complete(send_command(command, loop, status_window))
+            except curses.error:
+                if time.time() > next_distance_time:
+                    next_distance_time = time.time() + UPDATE_DISTANCE_EVERY
+                    loop.run_until_complete(send_command("distance", loop, distance_window))
+    finally:
+        loop.close()
+
+
+curses.wrapper(main)
